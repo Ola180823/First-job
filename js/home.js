4ba9cf58465854d8d6b8a370bcb3b9b11216e48c
olga <ola180823@gmail.com>
2023-10-03 15:58:46 +0300

    js

---
+++ js/home.js
@@ -0,0 +1,13 @@
+const fs = require('fs-extra');
+
+fs.mkdirSync('papka1');
+fs,fs.writeFileSync('papka1/myfile.txt', 'HELLO!');
+fs.mkdirSync('papka2');
+fs.renameSync('./myfile.txt,papka1,/papka2');
+fs.mkdirSync('papka3');
+fs.copySync('/papka2/myfile.txt', '/papka3/myfile.txt');
+fs.removeSync('/tmp/myfile.txt');
+fs.rmdirSync('papka1/,papka2/,papka3');
+console.log('success!')(err); 
+console.error(err);
+/
