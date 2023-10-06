3d26de1210e30ea14acd9ccdbf8626a3a05d7d44
OLA <ola180823@gmail.com>
2023-10-06 10:38:10 +0300

    js

---
+++ js/home2.js
@@ -0,0 +1,48 @@
+const { boolean, string } = require("yargs");
+ 
+
+let a=2;
+b=7;
+c="10склеит";
+console.log (a+b+c);
+
+let x=(2*8+4);
+console.log(x);
+
+
+const hello = 'привет мир';
+console.log(hello);
+
+const number = (5<6>8 );
+console.log (number);
+
+const m=("8"-0<5);
+console.log (m);
+
+console.log (2+true);
+console.log("hello" + " world");
+console.log(221+"ola"); 
+console.log("ola "+ false);
+
+console.log("25"/2);
+console.log("ola"/ false);
+console.log(50/0);
+console.log(2,0/true);
+
+console.log("мир"*10);
+console.log("888"*false);
+console.log("888"*true);
+console.log(44*true);
+console.log(44*false);
+
+//number to string
+let value = String(10+70);
+console.log(value);
+console.log(typeof value);
+
+//boolean to string
+let value2 = String(true);
+console.log(value2);
+console.log(typeof value2)
+
+
---
+++ js/home3.js
@@ -0,0 +1,5 @@
+let a=0;
+while(a<10);{    
+    console.log(f);
+    a++;
+}
