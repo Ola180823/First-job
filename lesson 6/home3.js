c9823f3e8714a742c6823e6f427a62a002968aad
OLA <ola180823@gmail.com>
2023-10-11 18:02:37 +0300

    lesson6  homework

---
+++ lesson 6/home3.js
@@ -0,0 +1,69 @@
+
+
+//#3(a)
+const arr = [];
+function getRandomIntInclusive(min, max)  {
+  min = (Math.min(1, 6));
+  max = (Math.max(1, 15));
+    return Math.floor(Math.random()* (max- min + 1)) + min;
+    result.push(Math.random);
+    total -= randomnumber;
+    const number = 15;
+    array.length =3;
+}
+console.log(Math.random());
+ 
+//#3(B)
+function getRandomFloat(min, max) {
+        min = (Math.ceil(1, 15));
+        max = (Math.floor(1, 15));
+        return Math.random() * 100/100;
+    }
+   console.log(Math.random());
+
+//#2
+
+let startDate = new Date('2000-01-01');  
+let endDate = new Date('2023-10-10');
+let count = 0;
+function countFriday13() {
+  while(startDate <= endDate) {
+    if (startDate.getDate() === 13)
+    (startDate.getDay() === 5) 
+      count ++;
+    }
+    startDate.setMonth.startDate.getMonth()+ 1;
+  }
+  return count;
+
+ console.log(countFriday13());
+ 
+   
+//#1
+//function throwcube {
+//return Math.floor(Math.random() * 6) + 1
+//}
+//let player1 = 0;
+//let player2 = 0;
+//for(let i = 0; i < 3; i++ ){
+  //result
+  //const  player1 = throwcube(); 
+  //const  player2 = throwcube();
+  //sum = (result (player1 +=  player2) )
+//}
+//if(sum (player1) > sum  (player2)){
+  //console.log(player1)
+//} else if 
+ // (sum (player2) > sum (player1) )
+  //console.log(player2);
+//} else  if {
+//console.log (Ничья!)
+//}
+
+
+
+
+
+
+
+ 
